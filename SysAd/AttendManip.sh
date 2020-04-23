e9a6db4c14b89cd4ceea17b0903901d1b3d819c5
A S Vignesh <59014425+vEdha-varman@users.noreply.github.com>
2020-04-23 17:08:26 +0530

    Basically Works
    Apart from anacron and .bashrc part, rest is done

--- SysAd/AttendManip.sh
+++ SysAd/AttendManip.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+
 Date=$(date +"%F")
 
-for g in Army Navy AirForce
-sudo cd ~ArmyGeneral
-for n in {1..50}
-    do
-    sudo echo "Date Post" > position_alloted.txt
-    pos=$(grep -i "$Date.* $g$n " position.log | awk '{print $3,$4}')
-    sudo echo "$Date $pos" >> position_alloted.txt
-done
+sudo touch /home/ArmyGeneral/attendance_record.txt
+echo "$Date:" | sudo tee -a /home/ArmyGeneral/attendance_record.txt
+grep -i "$Date.* Army" attendance.log | awk '{if ( $3 == "YES" ) print $2}' | sudo tee -a /home/ArmyGeneral/attendance_record.txt
+
+sudo touch /home/NavyMarshal/attendance_record.txt
+echo "$Date:" | sudo tee -a /home/NavyMarshal/attendance_record.txt
+grep -i "$Date.* Navy" attendance.log | awk '{if ( $3 == "YES" ) print $2}' | sudo tee -a /home/NavyMarshal/attendance_record.txt
+
+sudo touch /home/AirForceChief/attendance_record.txt
+echo "$Date:" | sudo tee -a /home/AirForceChief/attendance_record.txt
+grep -i "$Date.* AirForce" attendance.log | awk '{if ( $3 == "YES" ) print $2}' | sudo tee -a /home/AirForceChief/attendance_record.txt
