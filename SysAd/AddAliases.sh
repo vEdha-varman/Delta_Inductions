2ef73fc69fce589605fc5dc70cd83b6a4c592846
A S Vignesh <59014425+vEdha-varman@users.noreply.github.com>
2020-04-23 17:34:14 +0530

    .bashrc part done
    Just a basic file append

---
+++ SysAd/AddAliases.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# to add aliases
+
+echo "alias userGenerate='chmod +x ./UserManip.sh; ./UserManip.sh'" >>~/.bashrc
+echo "alias permit='chmod +x ./PermitManip.sh; ./PermitManip.sh'" >>~/.bashrc
+echo "alias autoSchedule='chmod +x ./PosManip.sh; ./PosManip.sh'" >>~/.bashrc
+echo "alias attendance='chmod +x ./AttendManip.sh; ./AttendManip.sh'" >>~/.bashrc
+echo "alias record='chmod +x ./RecordManip.sh; ./RecordManip.sh'" >>~/.bashrc
+
